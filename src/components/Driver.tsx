b7f6a1864560c706474137cd6c7ca696e6a8b651
RAF Barbosa <rcbar89@gmail.com>
2024-09-14 22:09:47 -0700

    pegando dados do db

--- src/components/Driver.tsx
+++ src/components/Driver.tsx
@@ -1,17 +1,23 @@
+import GenericDriver from '/src/assets/logosemfundo.png'
 
 interface DriverProps {
     name: string;
     team: string;
     driverNumber: string;
-    photo: string;
+    photo: { url: string } | null;
 }
 
 export function Driver(props: DriverProps) {
+    const defaultPhoto = GenericDriver;
 
     return (
         <div className="group">
             <span>
-                <img src={props.photo.url} alt={`${props.name} photo`} className="w-200 h-200 object-cover" />
+                <img
+                    src={props.photo ? props.photo.url : defaultPhoto} // Use default photo if photo is null
+                    alt={`${props.name} photo`}
+                    className="w-200 h-200 object-cover"
+                />
             </span>
             <header className="flex items-center justify-between mt-2">
                 <div className='flex flex-col'>
