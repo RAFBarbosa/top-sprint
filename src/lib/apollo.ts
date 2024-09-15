c9ddaa9ca2d0a2c6216af6cf6beb336a365f80ec
RAF Barbosa <rcbar89@gmail.com>
2024-09-14 17:12:15 -0700

    funfando pela primeira vez

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+console.log("API Access Token:", import.meta.env.VITE_API_ACCESS_TOKEN);
+
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL,
-  headers: {
-    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
-  },
-  cache: new InMemoryCache()
+	uri: import.meta.env.VITE_API_URL,
+	headers: {
+		Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+	},
+	cache: new InMemoryCache(),
 });
