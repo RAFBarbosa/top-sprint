84fcfe554808de696af571c2d2e97f5b3693ce39
RAF Barbosa <rcbar89@gmail.com>
2024-09-22 20:14:49 -0700

    all working good. Styling...

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,12 @@
 interface BannerProps {
+	title: string;
 	photo: { url: string };
 }
 
 export function Banner(props: BannerProps) {
 	return (
 		<div className="w-full">
+			<div className="text-3xl font-bold my-4">{props.title}</div>
 			<img
 				src={props.photo?.url}
 				alt={`${props.photo} photo`}
--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -11,15 +11,13 @@ export function Banners() {
 
 	return (
 		<aside className="md:w-1/2 mb-4 md:mb-0 border-t-8 border-r-8 border-f1-red rounded-tr-3xl">
-			<p className="text-3xl font-bold my-4">
-				Venha fazer parte da Top Sprint
-			</p>
 			<div className="pr-2 pb-4 md:pb-0">
 				{/* <Carousel> */}
 				{data?.banners && data.banners.length > 0 ? (
 					data.banners.map((data) => (
 						<Banner
 							key={data.id}
+							title={data.title || ""}
 							photo={data.photo || { url: GenericLogo }}
 						/>
 					))
