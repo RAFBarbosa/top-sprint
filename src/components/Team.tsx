7702097b0ecca5c0cbedb159eed3889224837d37
RAF Barbosa <rcbar89@gmail.com>
2024-09-28 00:01:53 -0700

    skeleton home

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,5 +1,5 @@
-import { Skeleton } from "@mui/material";
 import { useState } from "react";
+import { Skeleton } from "@mui/material";
 
 interface TeamProps {
 	name: string;
@@ -8,29 +8,33 @@ interface TeamProps {
 }
 
 export function Team(props: TeamProps) {
-	const [loading, setLoading] = useState(true);
+	const [loaded, setLoaded] = useState(false); // Track whether the image is loaded
 
 	const handleImageLoad = () => {
-		setLoading(false);
+		setLoaded(true); // Set to true when the image has finished loading
 	};
 
 	return (
-		<div className="mr-3">
-			{loading && (
+		<div
+			className={`mr-3 w-full h-auto rounded-lg shadow-lg mt-3 relative ${
+				loaded ? "" : "bg-gray-300" // Set background color while loading
+			}`}
+		>
+			{!loaded && (
 				<Skeleton
-					variant="rounded"
+					variant="rectangular"
 					width="100%"
-					height={410}
-					className="relative"
+					height={200} // Adjust height as needed
+					className="rounded-lg"
 				/>
 			)}
 			<img
 				src={props.photo?.url}
 				alt={`${props.name} photo`}
-				className={`w-full h-auto object-cover rounded-lg shadow-lg mt-3 ${
-					loading ? "hidden" : "block"
+				className={`w-full h-auto object-cover rounded-lg transition-opacity duration-500 ${
+					loaded ? "opacity-100" : "opacity-0" // Smooth transition to visible image
 				}`}
-				onLoad={handleImageLoad}
+				onLoad={handleImageLoad} // Trigger image load event
 			/>
 		</div>
 	);
