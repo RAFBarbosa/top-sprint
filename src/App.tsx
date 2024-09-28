bc05e2c3e018ad5e839858c2cc914ef7ce98c1d5
RAF Barbosa <rcbar89@gmail.com>
2024-09-27 23:05:10 -0700

    skeleton home

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Router } from "./Router";
-import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
 	return (
 		<div>
 			<ApolloProvider client={client}>
-				<SkeletonTheme baseColor="#202020" highlightColor="#444">
-					<BrowserRouter>
-						<Header />
-						<Router />
-						<Footer />
-					</BrowserRouter>
-				</SkeletonTheme>
+				<BrowserRouter>
+					<Header />
+					<Router />
+					<Footer />
+				</BrowserRouter>
 			</ApolloProvider>
 		</div>
 	);
--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -1,27 +1,36 @@
 import { useGetBannersQuery } from "../graphql/generated";
 import GenericLogo from "/src/assets/logosemfundo.png";
 import { Banner } from "./Banner";
-import Skeleton from "react-loading-skeleton";
-import { useEffect, useState } from "react";
+import { Skeleton } from "@mui/material";
 
-// const wrapped1 = <Skeleton wrapper={Banners} count={5} />;
+const loadingSkeleton = () => {
+	return (
+		<div className="md:w-1/2 mb-4 md:mb-0">
+			<div className="my-4 mr-3">
+				<Skeleton animation="wave" variant="text" height={30} />
+				<Skeleton animation="wave" variant="text" height={30} />
+				<Skeleton animation="wave" variant="text" height={30} />
+				<Skeleton
+					animation="wave"
+					variant="rectangular"
+					height={260}
+					sx={{ my: 1 }}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export function Banners() {
 	const { data, error, loading } = useGetBannersQuery();
 
-	if (loading) return <div>Carregando...</div>;
+	if (loading) return loadingSkeleton();
 	if (error) return <div>Erro: {error.message}</div>;
-
-	// const [loadingSkeleton, setLoading] = useState(true);
-
-	// useEffect(() => {
-	// 	setTimeout(() => setLoading(false), 1000);
-	// }, []);
 
 	return (
 		<aside className="md:w-1/2 mb-4 md:mb-0 border-t-8 border-r-8 border-f1-red rounded-tr-3xl">
 			<div className="pr-2 pb-4 md:pb-0">
-				{/* {data?.banners && data.banners.length > 0 ? (
+				{data?.banners && data.banners.length > 0 ? (
 					data.banners.map((data) => (
 						<Banner
 							key={data.id}
@@ -31,16 +40,8 @@ export function Banners() {
 						/>
 					))
 				) : (
-					<Skeleton />
-				)} */}
-				{data?.banners.map((data) => (
-					<Banner
-						key={data.id}
-						link={data.link || ""}
-						title={data.title || ""}
-						photo={data.photo || { url: GenericLogo }}
-					/>
-				))}
+					<p>Sem banners para carregar</p>
+				)}
 			</div>
 			<div className="md:hidden h-2 bg-divider bg-cover opacity-10 mr-2"></div>
 		</aside>
