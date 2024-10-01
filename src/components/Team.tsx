d1550a15ae5d6bb4e46f20679affa72bf996f3cd
RAF Barbosa <rcbar89@gmail.com>
2024-10-01 08:36:05 -0700

    skeleton home

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -8,23 +8,23 @@ interface TeamProps {
 }
 
 export function Team(props: TeamProps) {
-	const [loading, setLoading] = useState(true); // Track loading state
+	const [loading, setLoading] = useState(true);
 
 	const handleImageLoad = () => {
-		setLoading(false); // Set loading to false when the image has loaded
+		setLoading(false);
 	};
 
 	return (
 		<div
 			className={`mr-3 relative h-[410px] mt-2${
-				loading ? "bg-gray-300" : "" // Apply background color while loading
+				loading ? "bg-f1-lightSilver" : ""
 			}`}
 		>
 			{loading && (
 				<Skeleton
 					variant="rounded"
 					width="100%"
-					height="100%" // You can adjust the height as per the layout
+					height="100%"
 					className="absolute top-0 left-0"
 				/>
 			)}
@@ -32,9 +32,9 @@ export function Team(props: TeamProps) {
 				src={props.photo?.url}
 				alt={`${props.name} photo`}
 				className={`w-full h-auto object-cover rounded-lg shadow-lg transition-opacity duration-500 ${
-					loading ? "opacity-0" : "opacity-100" // Smooth transition from loading to visible
+					loading ? "opacity-0" : "opacity-100"
 				}`}
-				onLoad={handleImageLoad} // Trigger image load event
+				onLoad={handleImageLoad}
 			/>
 		</div>
 	);
