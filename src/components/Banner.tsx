f7c1fd4c9fecd86fa7fb0e1492ce416c5f7dc985
RAF Barbosa <rcbar89@gmail.com>
2024-10-03 09:10:28 -0700

    banner title

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,21 +6,26 @@ interface BannerProps {
 
 export function Banner(props: BannerProps) {
 	return (
-		<div className="w-full md:pr-3 pt-2 cursor-pointer text-lg hover:underline group">
+		<div className="w-full md:pr-3 pt-2 cursor-pointer text-lg group">
 			<a
 				href={props.link || ""}
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<div className="text-3xl md:text-4xl font-bold my-4 leading-8">
-					{props.title}
+				<div className="text-sm font-bold mt-2 text-f1-red uppercase">
+					Assista a corrida
 				</div>
-				<div className="overflow-hidden">
-					<img
-						src={props.photo?.url}
-						alt={`${props.photo} photo`}
-						className="shadow-lg transform transition-transform duration-150 group-hover:scale-105"
-					/>
+				<div className="group-hover:underline">
+					<div className="text-3xl md:text-4xl font-bold mb-4 leading-8">
+						{props.title}
+					</div>
+					<div className="overflow-hidden">
+						<img
+							src={props.photo?.url}
+							alt={`${props.photo} photo`}
+							className="shadow-lg transform transition-transform duration-150 group-hover:scale-110"
+						/>
+					</div>
 				</div>
 			</a>
 		</div>
