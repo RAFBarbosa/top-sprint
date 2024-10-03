894b41193211efd3c81c2e24ed0b0740043fe190
RAF Barbosa <rcbar89@gmail.com>
2024-10-03 09:03:04 -0700

    skeleton done + favicon

--- src/components/HallOfFame.tsx
+++ src/components/HallOfFame.tsx
@@ -1,16 +1,39 @@
+import { Skeleton } from "@mui/material";
+import { useState } from "react";
+
 interface HallOfFameProps {
 	season: string;
 	photo: { url: string };
 }
 
 export function HallOfFame(props: HallOfFameProps) {
+	const [loading, setLoading] = useState(true);
+
+	const handleImageLoad = () => {
+		setLoading(false);
+	};
+
 	return (
-		<div className="mr-3">
-			{/* <span className="">{props.season}</span> */}
+		<div
+			className={`mr-3 relative h-auto mt-2${
+				loading ? "bg-f1-lightSilver" : ""
+			}`}
+		>
+			{loading && (
+				<Skeleton
+					variant="rounded"
+					width="100%"
+					height="100%"
+					className="absolute top-0 left-0"
+				/>
+			)}
 			<img
 				src={props.photo?.url}
 				alt={`${props.season} photo`}
-				className="w-full h-auto object-cover rounded-lg"
+				className={`w-full h-auto object-cover rounded-lg shadow-lg transition-opacity duration-500 ${
+					loading ? "opacity-0" : "opacity-100"
+				}`}
+				onLoad={handleImageLoad}
 			/>
 		</div>
 	);
