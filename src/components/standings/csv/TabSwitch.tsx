87f7aea16dc213164f5d8b9d6be771c44cdcb3a8
RAF Barbosa <rcbar89@gmail.com>
2024-12-08 21:50:23 -0800

    tab button underline resized

--- src/components/standings/csv/TabSwitch.tsx
+++ src/components/standings/csv/TabSwitch.tsx
@@ -6,11 +6,11 @@ interface TabSwitchProps {
 }
 
 const TabSwitch: React.FC<TabSwitchProps> = ({ activeTab, setActiveTab }) => (
-	<div className="flex items-center space-x-1 underline-offset-16 font-semibold text-lg">
+	<div className="flex items-center space-x-1 underline-offset-15 font-semibold text-lg">
 		<button
 			className={`cursor-pointer tracking-wider px-5 ${
 				activeTab === "drivers"
-					? "underline decoration-f1-red decoration-6"
+					? "underline decoration-f1-red decoration-3"
 					: "no-underline"
 			}`}
 			onClick={() => setActiveTab("drivers")}
@@ -21,7 +21,7 @@ const TabSwitch: React.FC<TabSwitchProps> = ({ activeTab, setActiveTab }) => (
 		<button
 			className={`cursor-pointer tracking-wider px-5 ${
 				activeTab === "teams"
-					? "underline decoration-f1-red decoration-6"
+					? "underline decoration-f1-red decoration-3"
 					: "no-underline"
 			}`}
 			onClick={() => setActiveTab("teams")}
