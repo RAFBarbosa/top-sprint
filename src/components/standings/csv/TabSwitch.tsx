a637b707c18114912257c722b7595bca5dbd43f5
RAF Barbosa <rcbar89@gmail.com>
2024-12-09 01:39:27 -0800

    red underline tickness

--- src/components/standings/csv/TabSwitch.tsx
+++ src/components/standings/csv/TabSwitch.tsx
@@ -8,22 +8,26 @@ interface TabSwitchProps {
 const TabSwitch: React.FC<TabSwitchProps> = ({ activeTab, setActiveTab }) => (
 	<div className="flex items-center space-x-1 underline-offset-15 font-semibold text-xl">
 		<button
-			className={`cursor-pointer tracking-wide px-5 ${
-				activeTab === "drivers"
-					? "underline decoration-f1-red decoration-3"
-					: "no-underline"
-			}`}
+			className="cursor-pointer tracking-wide px-5"
+			style={{
+				textDecoration: activeTab === "drivers" ? "underline" : "none",
+				textDecorationColor:
+					activeTab === "drivers" ? "#eb1c24" : "transparent", // Assuming 'f1-red' color as #F1C5C5, you can adjust
+				textDecorationThickness: "5px",
+			}}
 			onClick={() => setActiveTab("drivers")}
 		>
 			PILOTOS
 		</button>
 		<div className="border-l h-5 border-f1-text opacity-30"></div>
 		<button
-			className={`cursor-pointer tracking-wide px-5 ${
-				activeTab === "teams"
-					? "underline decoration-f1-red decoration-3"
-					: "no-underline"
-			}`}
+			className="cursor-pointer tracking-wide px-5"
+			style={{
+				textDecoration: activeTab === "teams" ? "underline" : "none",
+				textDecorationColor:
+					activeTab === "teams" ? "#eb1c24" : "transparent", // Adjust color similarly
+				textDecorationThickness: "5px",
+			}}
 			onClick={() => setActiveTab("teams")}
 		>
 			EQUIPES
