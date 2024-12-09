e72e77d2651a3a506de7e2899eab092d059e55dc
RAF Barbosa <rcbar89@gmail.com>
2024-12-08 23:40:43 -0800

    test img size

--- src/components/standings/csv/StandingCard.tsx
+++ src/components/standings/csv/StandingCard.tsx
@@ -116,8 +116,8 @@ const StandingCard: React.FC<StandingCardProps> = ({
 							style={{
 								objectFit: "cover",
 								width: isDrivers
-									? "100%" // Image width on drivers
-									: "150%", // Teams section width
+									? "auto" // Image width on drivers
+									: "100%", // Teams section width
 								height: isDrivers ? "100%" : "100%", // Teams height
 								maxWidth: "100%", // Prevent image from overflowing horizontally
 								maxHeight: "100%", // Prevent image from overflowing vertically
