ba54640573aa84e19bdb040500b9bc598ed18e40
RAF Barbosa <rcbar89@gmail.com>
2025-02-21 17:41:37 -0800

    new social icons and signature added to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,19 +1,21 @@
 import { Logo } from "./Logo";
+import { SocialIcon } from "react-social-icons";
+import { Signature } from "../utils/Signature";
 
 export function Footer() {
 	return (
 		<footer className="bg-f1-carbon py-6 text-white mt-auto">
 			<div className="flex flex-col-reverse md:flex-row justify-between items-center md:max-w-screen-xl mx-auto px-3">
-				<div className="flex flex-row items-center gap-2">
+				<div className="flex md:flex-col items-center md:items-start gap-2 md:gap-0">
 					<div className="md:w-[60px] w-[40px]">
 						<Logo />
 					</div>
-					<p className="font-light text-sm">
+					<p className="font-light text-sm md:-translate-y-2">
 						Top Sprint League - Todos os direitos reservados
 					</p>
 				</div>
-				<div className="md:hidden h-[.5px] w-full bg-f1-silver my-6"></div>
-				<a
+				<div className="md:hidden h-[.5px] w-full bg-f1-silver my-6" />
+				{/* <a
 					href="https://chat.whatsapp.com/EitAZ6kSIVVDkK4I0pfsIV"
 					target="_blank"
 					className="flex flex-col md:flex-row items-center gap-2 md:mb-0 text-center"
@@ -27,9 +29,46 @@ export function Footer() {
 							fillRule="evenodd"
 						></path>
 					</svg>
-					<p className="flex justify-center text-center"></p>
-					Entre em contato e participe da próxima temporada
-				</a>
+				</a> */}
+				<div className="flex flex-col items-center gap-4 md:gap-2">
+					<p className="flex justify-center text-center">
+						Entre em contato e participe da próxima temporada
+					</p>
+					<div className="md:self-end space-x-2">
+						<SocialIcon
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
+							network="whatsapp"
+							url="https://chat.whatsapp.com/EitAZ6kSIVVDkK4I0pfsIV"
+							bgColor="transparent"
+							style={{ height: 45, width: 45 }}
+						/>
+						<SocialIcon
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
+							network="instagram"
+							url="https://www.instagram.com/ligatopsprint/"
+							bgColor="transparent"
+							style={{ height: 45, width: 45 }}
+						/>
+						<SocialIcon
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
+							network="youtube"
+							url="https://www.youtube.com/@feliplayTb"
+							bgColor="transparent"
+							style={{ height: 45, width: 45 }}
+						/>
+					</div>
+					<div className="h-[.5px] w-[45px] bg-f1-silver my-2 self-end hidden md:block" />
+
+					<div className="self-end hidden md:block">
+						<Signature />
+					</div>
+				</div>
+				<div className="order-first w-full md:hidden">
+					<div className="md:hidden h-[.5px] bg-f1-silver my-6" />
+					<div className="self-end">
+						<Signature side="left" />
+					</div>
+				</div>
 			</div>
 		</footer>
 	);
---
+++ src/components/utils/Signature.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface SignatureProps {
+	side?: string;
+}
+
+export function Signature(props: SignatureProps) {
+	const signature = {
+		name: "Rafael Barbosa",
+		imageUrl:
+			"https://www.rafbarbosa.dev/assets/favicon-32x32-BhriZ6KG.png",
+		website: "https://rafbarbosa.dev",
+	};
+
+	return (
+		<a href={signature.website} target="_blank">
+			<div
+				className={`flex items-center justify-center group cursor-pointer opacity-80 hover:opacity-100 transition-all duration-200 ${
+					props.side === "left" && "flex-row-reverse"
+				}`}
+			>
+				{/* Text */}
+				<div className="overflow-hidden p-1">
+					<p className="text-white text-sm group-hover:text-amber-300 transition-all duration-400 transform md:translate-x-full group-hover:-translate-x-0 ease-in-out md:opacity-0 group-hover:opacity-100">
+						Desenvolvido por {signature.name}
+					</p>
+				</div>
+				{/* Image */}
+				<img
+					src={signature.imageUrl}
+					alt={signature.name}
+					className="w-8 h-auto rounded-full p-1 bg-neutral-700 border border-white"
+				/>
+			</div>
+		</a>
+	);
+}
+
+export default Signature;
