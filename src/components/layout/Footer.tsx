43007941207661c1e7ceaf01d195f36a0cc3bc8d
Raf Barbosa <63738669+RAFBarbosa@users.noreply.github.com>
2025-07-01 00:59:51 -0700

    Revert "Brazuka"

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,24 +4,24 @@ import { Signature } from "../utils/Signature";
 
 export function Footer() {
 	return (
-		<footer className="bg-f1-silver py-6 text-white mt-auto">
+		<footer className="bg-f1-carbon py-6 text-white mt-auto">
 			<div className="flex flex-col-reverse md:flex-row justify-between items-center md:max-w-screen-xl mx-auto px-3">
 				<div className="flex md:flex-col items-center md:items-start gap-2 md:gap-0">
 					<div className="md:w-[60px] w-[40px]">
 						<Logo />
 					</div>
 					<p className="font-light text-sm md:-translate-y-2">
-						Brazuka - Todos os direitos reservados
+						Top Sprint League - Todos os direitos reservados
 					</p>
 				</div>
-				<div className="md:hidden h-[.5px] w-full bg-f1-black/50 my-6" />
+				<div className="md:hidden h-[.5px] w-full bg-f1-silver my-6" />
 				<div className="flex flex-col items-center gap-4 md:gap-2">
 					<p className="flex justify-center text-center">
 						Entre em contato e participe da próxima temporada
 					</p>
 					<div className="md:self-end space-x-2">
 						<SocialIcon
-							className="bg-f1-silver rounded-lg hover:bg-f1-black transition-colors duration-200 border-1 border-f1-black/50"
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
 							network="whatsapp"
 							target="_blank"
 							url="https://chat.whatsapp.com/BBUq88qF23DFffFN7mlRz1"
@@ -29,7 +29,7 @@ export function Footer() {
 							style={{ height: 45, width: 45 }}
 						/>
 						<SocialIcon
-							className="bg-f1-silver rounded-lg hover:bg-f1-black transition-colors duration-200 border-1 border-f1-black/50"
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
 							network="instagram"
 							target="_blank"
 							url="https://www.instagram.com/ligatopsprint/"
@@ -37,7 +37,7 @@ export function Footer() {
 							style={{ height: 45, width: 45 }}
 						/>
 						<SocialIcon
-							className="bg-f1-silver rounded-lg hover:bg-f1-black transition-colors duration-200 border-1 border-f1-black/50"
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
 							network="youtube"
 							target="_blank"
 							url="https://www.youtube.com/@ligatopsprint"
@@ -45,7 +45,7 @@ export function Footer() {
 							style={{ height: 45, width: 45 }}
 						/>
 						<SocialIcon
-							className="bg-f1-silver rounded-lg hover:bg-f1-black transition-colors duration-200 border-1 border-f1-black/50"
+							className="bg-f1-carbon rounded-lg hover:bg-f1-silver transition-colors duration-200 border-1 border-f1-silver"
 							network="discord"
 							target="_blank"
 							url="https://discord.gg/tZs5hwsubQ"
@@ -53,14 +53,14 @@ export function Footer() {
 							style={{ height: 45, width: 45 }}
 						/>
 					</div>
-					<div className="h-[.5px] w-[45px] bg-f1-black/50 my-2 self-end hidden md:block" />
+					<div className="h-[.5px] w-[45px] bg-f1-silver my-2 self-end hidden md:block" />
 
 					<div className="self-end hidden md:block">
 						<Signature />
 					</div>
 				</div>
 				<div className="order-first w-full md:hidden">
-					<div className="md:hidden h-[.5px] bg-f1-black/50 my-6" />
+					<div className="md:hidden h-[.5px] bg-f1-silver my-6" />
 					<div className="self-end">
 						<Signature side="left" />
 					</div>
--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,5 +1,5 @@
 import LogoSemFundo from "/src/assets/img/white-logo.png";
 
 export function Logo() {
-	return <img src={LogoSemFundo} className="" alt="Logo Brazuka" />;
+	return <img src={LogoSemFundo} className="" alt="Logo Top Sprint" />;
 }
