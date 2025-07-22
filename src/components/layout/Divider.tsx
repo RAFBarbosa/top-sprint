1237b7f3a7b3356d08a342453e38694fd1a0a34a
Raf Barbosa <63738669+RAFBarbosa@users.noreply.github.com>
2025-07-22 13:20:35 -0700

    Update Divider.tsx

--- src/components/layout/Divider.tsx
+++ src/components/layout/Divider.tsx
@@ -15,7 +15,7 @@ export function Divider({
 	className = "",
 }: DividerProps) {
 	return (
-		<div className=`relative my-4 h-18 w-full ${className}`>
+		<div className={`relative my-4 h-18 w-full ${className}`}>
 			<div className="h-full w-full bg-divider bg-cover opacity-5" />
 			<a
 				href={linkUrl}
