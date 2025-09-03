b4570de16f11de6680fd440c1940617a3514ad71
RAF Barbosa <rcbar89@gmail.com>
2025-09-02 20:05:06 -0700

    cheeky test to be removed

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,4 +1,4 @@
-import LogoSemFundo from "/src/assets/img/white-logo.png";
+import LogoSemFundo from "/src/assets/img/tsl.png";
 
 export function Logo() {
 	return <img src={LogoSemFundo} className="" alt="Logo Top Sprint" />;
