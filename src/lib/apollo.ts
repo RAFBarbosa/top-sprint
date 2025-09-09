11ed57eab2b0037dc19637a428dfd2587f31f8fb
RAF Barbosa <rcbar89@gmail.com>
2025-09-08 23:08:58 -0700

    old apollo back

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,17 +1,16 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
-const createClient = (token: string) =>
-	new ApolloClient({
-		link: createUploadLink({
-			uri: import.meta.env.VITE_API_URL,
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		}),
-		cache: new InMemoryCache(),
-	});
+// Create an upload link for file uploads
+const uploadLink = createUploadLink({
+	uri: import.meta.env.VITE_API_URL, // Your GraphQL endpoint
+	headers: {
+		Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+	},
+});
 
-export const publicClient = createClient(import.meta.env.VITE_API_PUBLIC_TOKEN);
-
-export const adminClient = createClient(import.meta.env.VITE_API_ADMIN_TOKEN);
+// Create the Apollo Client instance
+export const client = new ApolloClient({
+	link: uploadLink, // Use the upload link
+	cache: new InMemoryCache(),
+});
