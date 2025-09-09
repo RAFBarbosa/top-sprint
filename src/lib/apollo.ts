50fad6d564544d3e6a894d6d17bd8be9693a4a97
RAF Barbosa <rcbar89@gmail.com>
2025-09-08 22:52:29 -0700

    new admin logic

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,26 +1,17 @@
-// import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-// export const client = new ApolloClient({
-// 	uri: import.meta.env.VITE_API_URL,
-// 	headers: {
-// 		Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
-// 	},
-// 	cache: new InMemoryCache(),
-// });
-
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
-// Create an upload link for file uploads
-const uploadLink = createUploadLink({
-	uri: import.meta.env.VITE_API_URL, // Your GraphQL endpoint
-	headers: {
-		Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
-	},
-});
+const createClient = (token: string) =>
+	new ApolloClient({
+		link: createUploadLink({
+			uri: import.meta.env.VITE_API_URL,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}),
+		cache: new InMemoryCache(),
+	});
 
-// Create the Apollo Client instance
-export const client = new ApolloClient({
-	link: uploadLink, // Use the upload link
-	cache: new InMemoryCache(),
-});
+export const publicClient = createClient(import.meta.env.VITE_API_PUBLIC_TOKEN);
+
+export const adminClient = createClient(import.meta.env.VITE_API_ADMIN_TOKEN);
