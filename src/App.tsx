0e7faa2277e7ab9fd90c954830d6b7c34b173d0f
RAF Barbosa <rcbar89@gmail.com>
2026-03-10 23:03:16 -0700

    scroll to top behavior added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./lib/apollo";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
 import { Router } from "./Router";
 import { Analytics } from "@vercel/analytics/react";
 import { TabProvider } from "./contexts/TabContext";
 import { tenant } from "./shared/config/tenants";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+	const { pathname } = useLocation();
+	useEffect(() => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}, [pathname]);
+	return null;
+}
 
 function App() {
 	const style = Object.fromEntries(
@@ -17,6 +26,7 @@ function App() {
 		<div style={style} className="flex flex-col min-h-screen">
 			<ApolloProvider client={client}>
 				<BrowserRouter>
+					<ScrollToTop />
 					<TabProvider>
 						<Header />
 						<Router />
